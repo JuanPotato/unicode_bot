2f68da8fa69786fee0bd0c8e705f5f5cc9dfdebb
Juan Potato <hasantiny@gmail.com>
2017-02-19 20:30:04 -0500

    Linking names

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ extern crate phf;
 
 mod unicode;
 use unicode::UNICODE;
+use std::fmt::Write;
+
 
 fn main() {
-    println!("{}", get_char_names(" 🤳"));
+    println!("{}", get_char_names(" 123🤳"));
 }
 
 fn get_char_names(string: &str) -> String {
@@ -15,20 +17,27 @@ fn get_char_names(string: &str) -> String {
     let mut chars = string.chars();
         
     let (lower, _) = chars.size_hint();
-    let mut text = String::with_capacity(4 * lower);
-    // 3 for "\n" + 3 is the lowest the name could be
+    let mut text = String::with_capacity(23 * lower); // 24 is a magic number
 
-    text.push_str(UNICODE.get(&(chars.next().unwrap() as u32)).unwrap_or(&unknown));
- 
+    // [{ }](http://unic.gq/{:X})
+    //   ^  ^______________^ = 16 + ) and at least 1 hex = 18
+    //   |                             19 + 5 = 23
+    //   at least 3 + [] = 5
+
+    // unic.gq/hex_code_here is just a site I made for shorthanding
+    // http://www.fileformat.info/info/unicode/char/hex_code_here
+    
+    let first_c = chars.next().unwrap() as u32;
+    let _ = write!(text, "[{}](http://unic.gq/{:X})", 
+        UNICODE.get(&first_c).unwrap_or(&unknown),
+        first_c);
+
     for c in chars {
-        text.push_str("\n");
-        text.push_str(
-            match UNICODE.get(&(c as u32)) {
-                Some(text) => text,
-                None => "UNKNOWN CHARACTER",
-            }
-        );
+        let _ = write!(text, "\n[{}](http://unic.gq/{:X})", 
+            UNICODE.get(&(c as u32)).unwrap_or(&unknown),
+            c as u32);
     }
 
     text
 }
+
