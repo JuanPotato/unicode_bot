b4bf2fc1d831f36389970fd8e33fac4e847995c8
Tanuj <expectocode@gmail.com>
2021-10-31 20:29:48 +0000

    Update src/main.rs
    Co-authored-by: Hasan <hasan@juanpota.to>

--- src/main.rs
+++ src/main.rs
@@ -108,8 +108,6 @@ async fn handle_message(bot: Bot, msg: Message) {
                     } else {
                         let mut req =
                             SendMessage::new(msg.chat.id, messages::NEED_REPLY_TEXT_MESSAGE);
-                        req.parse_mode = ParseMode::Markdown;
-                        req.disable_web_page_preview = Some(true);
 
                         bot.send(&req).await.unwrap();
                     }
