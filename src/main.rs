0398ada1f1cf537ecb1bc019985070960afb87e6
Hasan Ibraheem <hasantiny@gmail.com>
2022-03-05 15:38:44 -0500

    Add basic stats

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use tg_botapi::types::{
     UpdateType,
 };
 use tg_botapi::Bot;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use std::collections::HashMap;
 
 mod messages;
 
@@ -56,31 +58,92 @@ async fn main() {
     run_bot(token).await;
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum StatisticAction {
+    Breakdown,
+    StartAbout,
+    Help,
+    Raw,
+    Filter,
+    Codepoint,
+    Unique,
+    BadCmd,
+    Inline,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Statistic {
+    user_id: i64,
+    action: StatisticAction,
+}
+
 async fn run_bot(token: String) {
     let bot = Bot::new(token);
 
     let mut updates = bot.start_polling();
 
+    let (stat_tx, stat_rx) = tokio::sync::mpsc::unbounded_channel();
+    tokio::spawn(stat_handler(stat_rx));
+
     while let Some(update) = updates.next().await {
         match update.update_type {
             UpdateType::Message(message) => {
-                tokio::spawn(handle_message(bot.clone(), message));
+                tokio::spawn(handle_message(bot.clone(), message, stat_tx.clone()));
             }
 
             UpdateType::InlineQuery(query) => {
-                tokio::spawn(handle_inline_query(bot.clone(), query));
+                tokio::spawn(handle_inline_query(bot.clone(), query, stat_tx.clone()));
             }
             _ => {}
         }
     }
 }
 
-async fn handle_message(bot: Bot, msg: Message) {
+async fn stat_handler(mut rx: UnboundedReceiver<Statistic>) {
+    let mut action_stats = HashMap::new();
+    let mut user_stats = HashMap::new();
+    let mut last_hour = 0;
+
+    println!("Hour, Unique Users, Total Msgs, Breakdown, StartAbout, Help, Raw, Filter, Codepoint, Unique, BadCmd, Inline");
+
+    while let Some(stat) = rx.recv().await {
+        let stat_hour = (chrono::Utc::now().timestamp() / 3600) * 3600;
+
+        if stat_hour != last_hour && last_hour != 0 {
+            let total_unique = user_stats.len();
+            let total_messages = user_stats.values().sum::<i32>();
+            user_stats.clear();
+
+            let breakdown = action_stats.remove(&StatisticAction::Breakdown).unwrap_or(0);
+            let startabout = action_stats.remove(&StatisticAction::StartAbout).unwrap_or(0);
+            let help = action_stats.remove(&StatisticAction::Help).unwrap_or(0);
+            let raw = action_stats.remove(&StatisticAction::Raw).unwrap_or(0);
+            let filter = action_stats.remove(&StatisticAction::Filter).unwrap_or(0);
+            let codepoint = action_stats.remove(&StatisticAction::Codepoint).unwrap_or(0);
+            let unique = action_stats.remove(&StatisticAction::Unique).unwrap_or(0);
+            let badcmd = action_stats.remove(&StatisticAction::BadCmd).unwrap_or(0);
+            let inline = action_stats.remove(&StatisticAction::Inline).unwrap_or(0);
+
+            println!("{last_hour}, {total_unique}, {total_messages}, {breakdown}, {startabout}, {help}, {raw}, {filter}, {codepoint}, {unique}, {badcmd}, {inline}");
+        }
+
+        last_hour = stat_hour;
+
+        *action_stats.entry(stat.action).or_insert(0) += 1;
+        *user_stats.entry(stat.user_id).or_insert(0) += 1;
+    }
+}
+
+struct Response(String, StatisticAction);
+
+async fn handle_message(bot: Bot, msg: Message, stat_tx: UnboundedSender<Statistic>) {
     if msg.chat.chat_type == ChatType::Private {
         let msg_text = match msg.get_text() {
             Some(text) => text.as_str(),
             None => return,
         };
+
+        let user_id = msg.from.as_ref().map(|u| u.id).unwrap_or(0);
 
         let reply_text_option = if let Some(ref reply) = msg.reply_to_message {
             reply.get_text()
@@ -94,17 +157,18 @@ async fn handle_message(bot: Bot, msg: Message) {
             (msg_text, None)
         };
 
-        let response = match cmd {
-            "/start" | "/about" => Some(messages::ABOUT_MESSAGE.into()),
+        use StatisticAction::*;
+        let Response(response, action) = match cmd {
+            "/start" | "/about" => Response(messages::ABOUT_MESSAGE.into(), StartAbout),
 
-            "/help" => Some(messages::HELP_MESSAGE.into()),
+            "/help" => Response(messages::HELP_MESSAGE.into(), Help),
 
             // Reply to a message that was interpreted as a command to get its breakdown
             "/raw" => {
                 if let Some(reply_text) = reply_text_option {
-                    Some(get_char_names(reply_text.chars()))
+                    Response(get_char_names(reply_text.chars()), Raw)
                 } else {
-                    Some(messages::NEED_REPLY_MESSAGE.into())
+                    Response(messages::NEED_REPLY_MESSAGE.into(), BadCmd)
                 }
             }
 
@@ -116,15 +180,15 @@ async fn handle_message(bot: Bot, msg: Message) {
                         let reply = get_char_names(filtered_chars);
 
                         if !reply.is_empty() {
-                            Some(reply)
+                            Response(reply, Filter)
                         } else {
-                            Some(messages::FILTER_EXHAUSTIVE.into())
+                            Response(messages::FILTER_EXHAUSTIVE.into(), BadCmd)
                         }
                     } else {
-                        Some(messages::NO_FILTER.into())
+                        Response(messages::NO_FILTER.into(), BadCmd)
                     }
                 } else {
-                    Some(messages::NEED_REPLY_MESSAGE.into())
+                    Response(messages::NEED_REPLY_MESSAGE.into(), BadCmd)
                 }
             }
 
@@ -141,12 +205,12 @@ async fn handle_message(bot: Bot, msg: Message) {
                         .ok()
                         .and_then(char::from_u32);
                     if let Some(c) = unicode {
-                        Some(c.to_string())
+                        Response(c.to_string(), Codepoint)
                     } else {
-                        Some(messages::INVALID_CODEPOINT.into())
+                        Response(messages::INVALID_CODEPOINT.into(), BadCmd)
                     }
                 } else {
-                    Some(messages::NO_CODEPOINT.into())
+                    Response(messages::NO_CODEPOINT.into(), BadCmd)
                 }
             }
 
@@ -166,27 +230,27 @@ async fn handle_message(bot: Bot, msg: Message) {
                         }
                     }
 
-                    Some(get_char_names(unique.drain(0..)))
+                    Response(get_char_names(unique.drain(0..)), Unique)
                 } else {
-                    Some(messages::NEED_REPLY_MESSAGE.into())
+                    Response(messages::NEED_REPLY_MESSAGE.into(), BadCmd)
                 }
             }
 
             // Break down anything else
-            _ => Some(get_char_names(msg_text.chars())),
+            _ => Response(get_char_names(msg_text.chars()), Breakdown),
         };
 
-        if let Some(s) = response {
-            let mut req = SendMessage::new(msg.chat.id, s);
-            req.parse_mode = ParseMode::Markdown;
-            req.disable_web_page_preview = Some(true);
+        let mut req = SendMessage::new(msg.chat.id, response);
+        req.parse_mode = ParseMode::Markdown;
+        req.disable_web_page_preview = Some(true);
 
-            bot.send(&req).await.unwrap();
-        }
+        bot.send(&req).await.unwrap();
+
+        stat_tx.send(Statistic { user_id, action }).unwrap();
     }
 }
 
-async fn handle_inline_query(bot: Bot, query: InlineQuery) {
+async fn handle_inline_query(bot: Bot, query: InlineQuery, stat_tx: UnboundedSender<Statistic>) {
     if query.query.is_empty() {
         return;
     }
@@ -204,9 +268,16 @@ async fn handle_inline_query(bot: Bot, query: InlineQuery) {
     response.is_personal = Some(false);
 
     bot.send(&response).await.unwrap();
+
+    let user_id = query.from.id;
+
+    stat_tx.send(Statistic {
+        user_id,
+        action: StatisticAction::Inline,
+    }).unwrap();
 }
 
-fn get_char_names(chars: impl Iterator<Item = char>) -> String {
+fn get_char_names(chars: impl Iterator<Item=char>) -> String {
     let mut text = String::with_capacity(4096); // max telegram message length
     let mut entities = 0;
 
